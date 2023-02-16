d8c4a4c16a726df94df09a1582afec6316d1f422
Conor Mc Daid <101251593+Conor-Mc-Daid@users.noreply.github.com>
2023-02-16 17:12:23 +0000

    Employee SQL Script added

---
+++ Cinema/Shared/SQL Scripts/Employee-Insert.sql
@@ -0,0 +1,12 @@
+﻿USE CinemaDB;
+
+INSERT INTO Employees VALUES
+('Usher','Conor','Mc Daid','ConorMcDaid01','Password123'),
+('Ticket Agent','John','Doe','JohnDoe98','Password098'),
+('Ticket Agent','Louise','Smith','LouiseSmith99','Password992'),
+('Concession staff','James','Elves','JamesElves03','Password180'),
+('Concession staff','Ryan','Mc Cann','RyanMcCann02','Password147'),
+('Concession staff','Liam','Doherty','LiamDoherty00','Password942'),
+('Concession staff','Patrick','Mc Dermott','PatrickMcDermott04','Password109'),
+('Concession staff','Sean','Mc Laughlin','SeanMcLaughlin02','Password924'),
+('Projectionist','Sarah','Mullan','SarahMullan97','Password206')
