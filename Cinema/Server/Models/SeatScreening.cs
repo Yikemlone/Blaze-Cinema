5de9c85c744fc9876ff7e61568d51d35ba04e165
Mikey - Desktop <sirvictini@gmail.com>
2023-02-19 15:13:06 +0000

    Added SeatScreening table

---
+++ Cinema/Server/Models/SeatScreening.cs
@@ -0,0 +1,18 @@
+﻿namespace Cinema.Server.Models
+{
+    public class SeatScreening
+    {
+        public int ID { get; set; }
+        public bool Booked { get; set; }
+
+
+        public int SeatID { get; set; }
+        public Seat Seat { get; set; }
+
+        public int? BookingID { get; set; }
+        public Booking? Booking { get; set; }
+
+        public int ScreeningID { get; set; }
+        public Screening Screening { get; set; }
+    }
+}
