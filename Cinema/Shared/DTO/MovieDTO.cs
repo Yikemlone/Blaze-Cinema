5fc5081d9915b1c287c94f3464009584111ce815
Conor Mc Daid <101251593+Conor-Mc-Daid@users.noreply.github.com>
2023-03-13 16:07:26 +0000

    osdubsgdon

--- Cinema/Shared/DTO/MovieDTO.cs
+++ Cinema/Shared/DTO/MovieDTO.cs
@@ -5,9 +5,16 @@
         public int ID { get; set; }
         public string Name { get; set; } = string.Empty;
         public int Duration { get; set; }
+<<<<<<< Updated upstream
         public string Description { get; set; } = string.Empty;
         public string AgeRating { get; set; } = string.Empty;
         public string Trailer { get; set; } = string.Empty;
+=======
+        public string Description { get; set; }
+        public string AgeRating { get; set; }
+        public string Trailer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+>>>>>>> Stashed changes
 
         public List<ScreeningDTO>? Screenings { get; set; }
     }
---
+++ Cinema/Shared/DTO/SeastScreeningDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Shared.DTO
+{
+    public class SeastScreeningDTO
+    {
+        public int ID { get; set; }
+        public bool Booked { get; set; }
+        public int seatId { get; set; }
+    }
+}
