c6853d705b50f5b6fdd27b784a2edaaa69da47f1
shanemcguire96 <L00161625@student.lyit.ie>
2023-03-13 16:10:01 +0000

    fixing poor work

--- Cinema/Shared/DTO/SeatScreeningDTO.cs
+++ Cinema/Shared/DTO/SeatScreeningDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Cinema.Shared.DTO
 {
-    public class SeastScreeningDTO
+    public class SeatScreeningDTO
     {
         public int ID { get; set; }
         public bool Booked { get; set; }
