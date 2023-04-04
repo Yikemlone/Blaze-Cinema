7bb8b8d49ec1fe4c9680a49832cf7aef0e709663
Mikey - Desktop <sirvictini@gmail.com>
2023-04-04 22:50:17 +0100

    WIP - Working on email service

---
+++ Cinema.DataAccess/Services/EmailServices/EmailService.cs
@@ -0,0 +1,18 @@
+﻿using Cinema.DataAccess.Services.EmailService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.DataAccess.Services.EmailServices
+{
+    public class EmailService : IEmailService
+    {
+        public async Task SendEmail()
+        {
+            
+        }
+    }
+}
---
+++ Cinema.DataAccess/Services/EmailServices/IEmailService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mime;
+
+
+namespace Cinema.DataAccess.Services.EmailService
+{
+    public interface IEmailService
+    {
+        public Task SendEmail();
+    }
+
+}
+
