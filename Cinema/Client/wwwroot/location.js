8b7724fc13cea7118974819ac05da2ed84b684ea
shanemcguire96 <l00161625@student.lyit.ie>
2023-04-25 01:32:30 +0100

    added map to contact details

--- Cinema/Client/wwwroot/location.js
+++ Cinema/Client/wwwroot/location.js
@@ -1,6 +1,6 @@
-﻿export function load_map() {
-
-    let map = L.map("map").setView([54.95212256082817, -7.7209027774341905], 13);
+﻿export function load_map()
+{
+    let map = L.map("map").setView([54.95212256082817, -7.7209027774341905], 17);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
     var marker = L.marker([54.95245451, -7.72099947]).addTo(map);
     var polygon = L.polygon([
@@ -14,6 +14,6 @@
             fillOpacity: 0.25
         }).addTo(map);
 
-    marker.bindPopup("<b>Blaze Cinema</b>");
+    marker.bindPopup("<b>Blaze Cinema</b><br><b>Port Road</b><br><b>Letterkenny</b><br><b>Co.Donegal</b><br><b>F92 FC93</b>");
     return "";
 }
